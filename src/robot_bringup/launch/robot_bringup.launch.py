7a5dd5f4e5e73f1e35e748763dec83eae721a225
Justin Woodman <justin.woodman@gmail.com>
2025-10-19 00:58:18 -0400

    Renamed robot_description node to robot_bringup

--- src/robot_bringup/launch/robot_bringup.launch.py
+++ src/robot_bringup/launch/robot_bringup.launch.py
@@ -7,16 +7,22 @@ from launch_ros.substitutions import FindPackageShare
 import os
 
 def generate_launch_description():
-    pkg_share = FindPackageShare(package='robot_description').find('robot_description')
-    default_model_path = os.path.join(pkg_share, 'description', 'robot_description.urdf')
+    pkg_share = FindPackageShare(package='robot_bringup').find('robot_bringup')
+    default_model_path = os.path.join(pkg_share, 'urdf', 'robot_description.urdf')
 
     robot_state_publisher_node = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
-        parameters=[{'robot_description': Command(['xacro ', LaunchConfiguration('model')])}]
+        parameters=[{'robot_description': Command(['xacro ', LaunchConfiguration('model')])}],
+    )
+
+    ardupilot_tf_publisher_node = Node(
+        package='ardupilot_tf_publisher',
+        executable='ardupilot_tf_publisher',
     )
 
     return LaunchDescription([
         DeclareLaunchArgument(name='model', default_value=default_model_path, description='Absolute path to robot model file'),
         robot_state_publisher_node,
+        ardupilot_tf_publisher_node,
     ])
